0258681c57f7520494343abb3bf4e44ad99ef9e5
Ryousuke.Irie <ryou398w@gmail.com>
2025-11-22 14:42:27 +0900

    リザルト完成。

--- Assets/Irie/Scripts/ResultManager.cs
+++ Assets/Irie/Scripts/ResultManager.cs
@@ -1,11 +1,16 @@
-using System.Collections;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class ResultManager : MonoBehaviour
 {
     [SerializeField]
     private Text coolKanValueText;
+
+    [SerializeField]
+    private Text coolKanValueTextBack;
 
     [SerializeField]
     private int coolKanValue = 0;
@@ -21,6 +26,20 @@ public class ResultManager : MonoBehaviour
     [SerializeField]
     private float speed = 10.0f;
 
+    [SerializeField]
+    private GameObject Mask;
+
+    // [HideInInspector] 実行時にはこの文字列だけあれば良いのでインスペクタからは隠す
+    [HideInInspector]
+    [SerializeField] private string sceneToLoad;
+
+    // #if UNITY_EDITOR ～ #endif で囲まれた部分はエディタ上でのみ有効になる
+#if UNITY_EDITOR
+    // インスペクタに表示するためのSceneAsset型変数
+    [Header("遷移先シーン選択")] // インスペクタに見出しを表示
+    [SerializeField] private SceneAsset sceneAsset; // ここにシーンファイルをD&Dする
+#endif
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -31,8 +50,18 @@ public class ResultManager : MonoBehaviour
     void Update()
     {
         coolKanValueText.text = "冷やした缶 : " + coolKanValueCurrent + "本";
-        if(KanParent.transform.position.x > (6 - (coolKanValue - 3)) * 300)
+        coolKanValueTextBack.text = "冷やした缶 : " + coolKanValueCurrent + "本";
+        if (KanParent.transform.position.x > (6 - (coolKanValue - 3)) * 300)
+        {
             KanParent.transform.position += new Vector3(speed * Time.deltaTime, 0.0f, 0.0f);
+        }
+        if(coolKanValue ==  coolKanValueCurrent)
+        {
+            if(Keyboard.current.anyKey.wasPressedThisFrame)
+            {
+                SceneManager.LoadScene(sceneToLoad);
+            }
+        }
     }
 
     public void SetKanValue(int value)
@@ -53,5 +82,22 @@ public class ResultManager : MonoBehaviour
         coolKanValueCurrent++;
     }
 
-
+    // OnValidateメソッドもエディタ専用
+#if UNITY_EDITOR
+    // インスペクタで値が変更された時などに自動で呼ばれるメソッド
+    private void OnValidate()
+    {
+        // sceneAssetフィールドにシーンが設定されたら
+        if (sceneAsset != null)
+        {
+            // そのシーンの名前（文字列）を sceneToLoad 変数にコピーする
+            sceneToLoad = sceneAsset.name;
+        }
+        else
+        {
+            // SceneAssetが未設定なら文字列も空にする
+            sceneToLoad = "";
+        }
+    }
+#endif
 }
